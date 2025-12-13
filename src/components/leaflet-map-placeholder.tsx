ac20e22acbb5dc105c5b6bc13bd8b27b049c0eb2
Jacek Pietrzyk <jacpie3k@gmail.com>
2025-12-13 21:45:12 +0100

    first marker

--- src/components/leaflet-map-placeholder.tsx
+++ src/components/leaflet-map-placeholder.tsx
@@ -1,5 +1,5 @@
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import React from "react";
 
 const center = { lat: 51.505, lng: -0.09 };
@@ -13,6 +13,12 @@ const LeafletMapPlaceholder: React.FC = () => (
     pixelRatio={window.devicePixelRatio || 1}
   >
     <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+    <Marker position={{ lat: 51.51, lng: -0.1 }}>
+      <Popup>
+        Marker near London<br />
+        (51.51, -0.1)
+      </Popup>
+    </Marker>
   </MapContainer>
 );
 
